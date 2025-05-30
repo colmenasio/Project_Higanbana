71ee29e0eba54bb1e5dc8548d3ac503cd98139e9
Colmenero <a.colmenero.fernandez\@gmail.com>
2025-05-30 22:23:48 +0200

    [F] Not no number is displayed if there is only 1 item in the itemstack

--- ui/ItemStackDisplay/item_stack_display.gd
+++ ui/ItemStackDisplay/item_stack_display.gd
@@ -14,7 +14,7 @@ class_name ItemStackDisplay
 func display_stack(stack: ItemStack):
 	$TextureButton.texture_normal = stack.get_prototype()._base_texture
 	$TextureButton.texture_hover = load("res://assets/item/EmptyStack.png")
-	$Amount.text = "" if stack.is_type_empty() else String.num_int64(stack.get_amount())
+	$Amount.text = "" if stack.get_amount() in [0, 1] else String.num_int64(stack.get_amount())
 
 ## [param callback] must take a single argument of type CInteraction.Type
 func bind_callback(callback: Callable):
